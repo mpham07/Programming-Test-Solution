341ea50ace94cb41d962da45fca3ffe473544b68
Minh Pham <ducminh.it48@gmail.com>
2018-04-11 23:02:45 -0500

    done object 1 & 2

--- src/MainProg.java
+++ src/MainProg.java
@@ -8,25 +8,25 @@ public class MainProg {
 		
 		MainProg prog = new MainProg();
 		String randASCIIString = "cat";
-		String strDNA = prog.encodeTo(randASCIIString, Type.DNA);
-		String strRNA = prog.encodeTo(randASCIIString, Type.RNA);
+		String strDNA = prog.encodeASCIIToDNAorRNA(randASCIIString, Type.DNA);
+		String strRNA = prog.encodeASCIIToDNAorRNA(randASCIIString, Type.RNA);
 		System.out.println("DNA: " + randASCIIString + " -> " + strDNA);
 		System.out.println("RNA: " + randASCIIString + " -> " + strRNA);
 	}
 	
-	String encodeTo(String strAscii, Type type) {
+	String encodeASCIIToDNAorRNA(String strAscii, Type type) {
 		
 		String strReturn = "";
 		for(int i = 0 ; i < strAscii.length(); i++) {
 			char ch = strAscii.charAt(i);
 			int ascii_ch = (int) ch;
-			int[] num8bit = convertTobinary8bit(ascii_ch);
+			int[] num8bit = convertIntegerTobinary8bit(ascii_ch);
 
 			
 			int j = 0;
 			while(j < 8) {
 				
-				strReturn += convertToDNAorRNAchar(num8bit[j], num8bit[j+1], type); 
+				strReturn += convertASCIIToDNAorRNAchar(num8bit[j], num8bit[j+1], type); 
 				j += 2;
 			}
 		}
@@ -34,14 +34,14 @@ public class MainProg {
 		return strReturn;
 	}
 	
-	int[] convertTobinary8bit(int number) {
+	int[] convertIntegerTobinary8bit(int number) {
 	    int[] binary = new int[8];
 	    for (int i = 7, num = number; i >= 0; i--, num >>>= 1)
 	        binary[i] = num & 1;
 	    return binary;
 	}
 	
-	String convertToDNAorRNAchar(int first, int second, Type type) {
+	String convertASCIIToDNAorRNAchar(int first, int second, Type type) {
 		String chars[] = DNA_Char;
 		if (type == Type.RNA) { chars = RNA_Char; }
 		if(first == 0 && second == 0) return chars[0]; 
@@ -50,4 +50,6 @@ public class MainProg {
 		if(first == 1 && second == 0) return chars[2]; 
 		return "";
 	}
+	
+	//String convert
 }
